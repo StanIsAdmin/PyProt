ecda4cc5d492b92946c340a7b3b4f24de6f9b474
StanIsAdmin <sgueniff@ulb.ac.be>
2016-12-16 16:53:19 +0100

    Lines are important

--- src/main2.py
+++ src/main2.py
@@ -5,13 +5,16 @@ with open(r"../resources/dataset/CATH_info.txt") as infoFile:
 	with open(r"../resources/dataset/CATH_info-PARSED.txt", 'w') as outFile:
 		for line in infoFile.readlines():
 			d = DSSP(r"../resources/dataset/dssp/" + line[0:4] + ".dssp")
+			description = "> " + d.identifier + "|" + d.protein + "|" + d.organism
 			seq, struct = d.getSequenceStructure(line[4])
-			outFile.writelines(["> " + d.identifier + "|" + d.protein + "|" + d.organism,seq,struct])
+			
+			outFile.writelines(l + "\n" for l in [description,seq,struct])
 		
 with open(r"../resources/dataset/CATH_info_test.txt") as infoFile:
 	with open(r"../resources/dataset/CATH_info_test-PARSED.txt", 'w') as outFile:
 		for line in infoFile.readlines():
 			d = DSSP(r"../resources/dataset/dssp_test/" + line[0:4] + ".dssp")
+			description = "> " + d.identifier + "|" + d.protein + "|" + d.organism
 			seq, struct = d.getSequenceStructure(line[4])
-			outFile.writelines(["> " + d.identifier + "|" + d.protein + "|" + d.organism,seq,struct])
-		
+			
+			outFile.writelines(l + "\n" for l in [description,seq,struct])
