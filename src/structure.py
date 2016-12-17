fdc77b7ca30dd12b81390674de4c8b88393183c7
StanIsAdmin <sgueniff@ulb.ac.be>
2016-12-17 14:41:29 +0100

    Add some comments

--- src/structure.py
+++ src/structure.py
@@ -3,12 +3,18 @@ from aminoacid import AminoAcid
 
 
 class GOR3:
+	"""
+	Implements the GOR III secondary structure prediction algorithm.
+	Objects of this class must be trained with known examples of sequences and their structure
+	before being able to predict the structures of new sequences.
+	"""
+
 	def __init__(self):
 		self.structures = "HETC"
 		self.neighbourOffset = 8
 		
-		self.trainings = 0
-		self.structureCount = {s:0 for s in self.structures}
+		self.trainings = 0 #Number of trainings (one per AA)
+		self.strucCount = {s:0 for s in self.structures}
 		self.pairCount = {(s, a):0 for s in self.structures for a in AminoAcid.getAllNames()}
 		self.tripletCount = {}
 		for s in self.structures:
@@ -18,11 +24,15 @@ class GOR3:
 		
 		
 	def train(self, sequence, structure):
+		"""
+		Trains the system with a known example of a sequence and its structure.
+		"""
 		self.trainings += len(sequence)
+		
 		for index in range(len(sequence)):
 			curAminoacid = sequence[index]
 			curStructure = structure[index]
-			self.structureCount[curStructure] += 1
+			self.strucCount[curStructure] += 1
 			self.pairCount[(curStructure, curAminoacid)] += 1
 			
 			for neiAminoacid in self.neighbourValues(sequence, index):
@@ -30,16 +40,24 @@ class GOR3:
 	
 	
 	def predict(self, sequence):
+		"""
+		Returns the predicted structure of 'sequence', based on received training.
+		"""
 		structure = [] #Result: predicted structure
 		
+		#Predict structures for each aminoacid in sequence
 		for index in range(len(sequence)):
 			curAminoacid = sequence[index]
 			
-			predStructure = ""
-			predScore = -1
-			for curStructure in self.structures:
+			#First possible structure
+			predStructure = self.structures[0]
+			predScore = self.__getScore(sequence, index, predStructure)
+			
+			#Other structures
+			for curStructure in self.structures[1:]:
 				curScore = self.__getScore(sequence, index, curStructure)
 				
+				#Remember structure that gives best score
 				if curScore > predScore:
 					predStructure = curStructure
 					predScore = curScore
@@ -49,6 +67,9 @@ class GOR3:
 		
 	
 	def __getScore(self, sequence, index, struct):
+		"""
+		Returns I(deltaS, R) as defined by the GOR III algorithm.
+		"""
 		aminoacid = sequence[index]
 		scoreTerms = []
 		
@@ -56,7 +77,7 @@ class GOR3:
 		score /= sum(self.pairCount[(otherStruct, aminoacid)] for otherStruct in self.getStructures(struct))
 		scoreTerms.append(score)
 		
-		score = (self.trainings - self.structureCount[struct]) / self.structureCount[struct]
+		score = (self.trainings - self.strucCount[struct]) / self.strucCount[struct]
 		scoreTerms.append(score)
 		
 		for neiAminoacid in self.neighbourValues(sequence, index):
